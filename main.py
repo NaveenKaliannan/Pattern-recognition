6493d527be1c077a8b425e4eff264e31eca00648
Naveen Kumar Kaliannan <naveenkumar5892@gmail.com>
2022-02-25 11:20:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -171,11 +171,10 @@ plt.title('Training and Validation Loss')
 plt.show()
 
 
-img = tf.keras.utils.load_img("test.png", target_size=(img_height, img_width))
+img = tf.keras.utils.load_img("test2.png", target_size=(img_height, img_width))
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0) # Create a batch
 predictions = model.predict(img_array)
 score = tf.nn.softmax(predictions[0])
-print("This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
-)
-
+print("This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)) )
+   
